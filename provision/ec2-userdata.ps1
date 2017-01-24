dd9f6426f2ae34f931202ceb0c03aa8c374ca3be
imonthercks <imonthercks@gmail.com>
2017-01-23 23:51:52 -0600

    Moved provisioning to ec2-userdata.ps1 file in order to allow boxstarter to handle automatic reboots without conflicting with Packer.  Also added inline sysprep provisioner.

--- provision/ec2-userdata.ps1
+++ provision/ec2-userdata.ps1
@@ -1,5 +1,8 @@
 <powershell>
 
+# Set Time Zone
+Invoke-Expression "tzutil.exe /s 'Eastern Standard Time'"
+
 write-output "Running User Data Script"
 write-host "(host) Running User Data Script"
 
@@ -36,4 +39,25 @@ cmd.exe /c net stop winrm
 cmd.exe /c sc config winrm start= auto
 cmd.exe /c net start winrm
 
+# Install Chocolatey, BoxStarter and other reboot dependent dependencies
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+choco feature enable -n=allowGlobalConfirmation
+
+cinst Boxstarter
+
+cinst DotNet4.5
+
+#Enable Web Services
+cinst IIS-WebServerRole -source WindowsFeatures
+cinst IIS-ISAPIFilter -source WindowsFeatures
+cinst IIS-ISAPIExtensions -source WindowsFeatures
+
+#Enable ASP.NET on win 2012/8
+cinst IIS-NetFxExtensibility45 -source WindowsFeatures
+cinst NetFx4Extended-ASPNET45 -source WindowsFeatures
+cinst IIS-ASPNet45 -source WindowsFeatures
+
+#Install the applicable Windows Updates for the above installed packages
+Install-WindowsUpdate -getUpdatesFromMS -acceptEula
 </powershell>
--- provision/provision.ps1
+++ provision/provision.ps1
@@ -1,7 +1,1 @@
-# Set Time Zone
-Invoke-Expression "tzutil.exe /s 'Eastern Standard Time'"
-
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-
-choco feature enable -n=allowGlobalConfirmation
 
